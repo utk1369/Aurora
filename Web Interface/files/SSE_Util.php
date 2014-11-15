d4f39f0137e21121719cc71167fe2590afb13e2d
utk1369 <utkarsh1369.11@bitmesra.ac.in>
2014-11-15 15:50:21 +0530

    Create SSE_Util.php
    Utility class for Server Sent Events

---
+++ Web Interface/files/SSE_Util.php
@@ -0,0 +1,12 @@
+<?php
+/** @author: utk
+ */
+ class SSE_Util {	
+	public static function sendMessageToClient($message) {
+		header('Content-Type: text/event-stream');
+		header('Cache-Control: no-cache');
+		echo "data: $message".PHP_EOL.PHP_EOL;
+		ob_flush();
+		flush();
+	}	
+}
